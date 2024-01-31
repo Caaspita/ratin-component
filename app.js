b19d94b43a6663fcb0d3cc6011a59ee48c8cb20f
Caspita <lucascaspani@gmail.com>
2024-01-31 14:54:25 -0300

    app

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+const eleccion = 0;
 
+function btn(num){
 
-
-function submit() {
-    switch (rate){
-        case 1:
-            console.log('Elegiste 1');
-            break;
-        case 2:
-            console.log('Elegiste 2');
-            break;
+    if(num === 1){
+         const eleccion = 1;
+         console.log(eleccion)
+    }else if(num ===2){
+        const eleccion = 2;
+        console.log(eleccion)
+    }else if(num ===3){
+        const eleccion = 3;
+        console.log(eleccion)
+    }else if(num ===4){
+        const eleccion = 4;
+        console.log(eleccion)
+    }else if(num ===5){
+        const eleccion = 5;
+        console.log(eleccion)
     }
 }
 
+
+function submit(eleccion) {
+    if(eleccion != 0){
+        const numrate = document.getElementById('numrate');
+        numrate.innerText = 2;
+        window.location.href = 'rating.html';
+    }
+   
+}
