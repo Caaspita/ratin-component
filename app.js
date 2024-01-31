c454f78d3c4160ee78a7cfa6ffca4dae10a4835e
Caspita <lucascaspani@gmail.com>
2024-01-31 12:58:33 -0300

    Agrego js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+function submit() {
+    switch (rate){
+        case 1:
+            console.log('Elegiste 1');
+            break;
+        case 2:
+            console.log('Elegiste 2');
+            break;
+    }
+}
+
